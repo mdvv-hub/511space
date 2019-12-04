6a38b62861a6f41688b29a2c83d37ca5566cd77b
511 <511@DESKTOP-IDE8FD9>
2019-12-04 16:30:33 +0800

    511提交

---
+++ src/main/java/com/testGit/App.java
@@ -0,0 +1,13 @@
+package com.testGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
